cba1351969ab96ff6bacef5e5e7860a451c50498
Arun B Kumar <arun.b.aux@go-jek.com>
2022-09-19 13:35:57 +0530

    Added new test for reading nested class

--- src/test/java/com/testvagrant/yamlreader/tests/YamlBeansTest.java
+++ src/test/java/com/testvagrant/yamlreader/tests/YamlBeansTest.java
@@ -5,6 +5,8 @@ import com.google.inject.Inject;
 import com.testvagrant.yamlreader.YamlBeansFileReader;
 import com.testvagrant.yamlreader.io.ResourceFileReader;
 import com.testvagrant.yamlreader.tests.models.Employee;
+import com.testvagrant.yamlreader.tests.models.MessageField;
+import com.testvagrant.yamlreader.tests.models.VariableField;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,8 +21,8 @@ public class YamlBeansTest extends BaseTest {
     public void readAsAnObject() throws YamlException, FileNotFoundException {
         File file = resourceFileReader.getFile("data/employee.yml");
         Employee employee = new YamlBeansFileReader().read(file, Employee.class);
-        Assert.assertEquals(employee.getId(),"1");
-        Assert.assertEquals(employee.getAge(),35);
+        Assert.assertEquals(employee.getId(), "1");
+        Assert.assertEquals(employee.getAge(), 35);
     }
 
     @Test
@@ -33,7 +35,16 @@ public class YamlBeansTest extends BaseTest {
     @Test
     public void readByIgnoringDuplicateKeyByChangingConfig() throws YamlException, FileNotFoundException {
         File file = resourceFileReader.getFile("data/duplicatekey.yml");
-        Object read = new YamlBeansFileReader().readAsMapWithConfig(file, "address",true);
+        Object read = new YamlBeansFileReader().readAsMapWithConfig(file, "address", true);
         Assert.assertTrue(read.toString().contains("NYC"));
+    }
+
+    @Test
+    public void readByNestedDetails() throws YamlException, FileNotFoundException {
+        File file = resourceFileReader.getFile("data/variablefield.yml");
+        MessageField field = (MessageField) new YamlBeansFileReader().readAsNested(file, MessageField.class, "variableFields", VariableField.class);
+        for (VariableField variableField : field.getVariableFields()) {
+            Assert.assertEquals(variableField.getName(), "nestedField 1");
+        }
     }
 }
---
+++ src/test/java/com/testvagrant/yamlreader/tests/models/VariableField.java
@@ -0,0 +1,13 @@
+package com.testvagrant.yamlreader.tests.models;
+
+public class VariableField extends MessageField {
+    public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
